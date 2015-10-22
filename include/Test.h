03ff7bb395d6ffdb24d9f3f5083c4cf3c6d2d05c
Brian Craig <briancraigok@gmail.com>
2015-10-22 12:00:19 -0300

    de char* a string en los tests

--- include/Test.h
+++ include/Test.h
@@ -1,11 +1,14 @@
+#include <string>
+using std::string;
+
 #ifndef TEST_H
 #define TEST_H
 
 
 namespace Test{
-  void assert_equal(char*, int, int);
-  void assert_true(char*, bool);
-  void assert_false(char*, bool);
+  void assert_equal(string, int, int);
+  void assert_true(string, bool);
+  void assert_false(string, bool);
 }
 
 #endif // TEST_H
--- src/Test.cpp
+++ src/Test.cpp
@@ -3,29 +3,29 @@
 #include <string>
 using namespace std;
 
-void test(bool passed, char* name, std::string x, std::string y){
+void test(bool passed, string name, string x, string y){
     if (passed) cout << "Paso el test : " << name << " -> " << x << endl;
     else cout << "** Fallo **: " << name << " " << x << " != " << y << endl;
 }
 
-std::string to_s(int x){
-  return std::to_string(x);
+string to_s(int x){
+  return to_string(x);
 }
-std::string to_s(bool x){
+string to_s(bool x){
   if(x) return "true";
   else return "false";
 }
 
 namespace Test{
-void assert_equal(char* name, int x, int y)
+void assert_equal(string name, int x, int y)
 {
   test((x == y), name, to_s(x), to_s(y));
 }
-void assert_true(char* name, bool x)
+void assert_true(string name, bool x)
 {
   test((x == true), name, to_s(x), "true");
 }
-void assert_false(char* name, bool x)
+void assert_false(string name, bool x)
 {
   test((x == false), name, to_s(x), "false");
 }
